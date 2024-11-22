e966c0c1fdc1fe95f6f5bf5a0e3781b1264b75e2
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2024-11-22 12:44:09 -0500

    Update OrbEntity.kt

--- src/main/kotlin/me/fzzyhmstrs/orbs_n_stuff/entity/OrbEntity.kt
+++ src/main/kotlin/me/fzzyhmstrs/orbs_n_stuff/entity/OrbEntity.kt
@@ -1,5 +1,9 @@
 package me.fzzyhmstrs.orbs_n_stuff.entity
 
+import me.fzzyhmstrs.lootables.api.LootablesApi
+import me.fzzyhmstrs.lootables.api.LootableItem.LootableData.EventType
+import me.fzzyhmstrs.orbs_n_stuff.config.ONSConfig
+import me.fzzyhmstrs.orbs_n_stuff.entity.variant.OrbVariant
 import net.minecraft.entity.Entity
 import net.minecraft.entity.EntityType
 import net.minecraft.entity.Ownable
@@ -7,8 +11,10 @@ import net.minecraft.entity.data.DataTracker
 import net.minecraft.nbt.NbtCompound
 import net.minecraft.world.World
 
-class OrbEntity(type: EntityType<out OrbEntity>, world: World) : Entity(type, world), Ownable {
+class OrbEntity(type: EntityType<out OrbEntity>, world: World, private val variant: OrbVariant) : Entity(type, world), Ownable {
 
+    private var owner: Entity? = null
+    
     override fun initDataTracker(builder: DataTracker.Builder) {
         TODO("Not yet implemented")
     }
@@ -21,7 +27,25 @@ class OrbEntity(type: EntityType<out OrbEntity>, world: World) : Entity(type, wo
         TODO("Not yet implemented")
     }
 
+    override fun tick() {
+        super.tick
+        if (this.age > ONSConfig.INSTANCE.orbDespawnTime.get()) {
+            this.discard()
+        }
+    }
+
+    private fun handleCollision(collidedWith: Entity) {
+        if (collidedWith !is ServerPlayerEntity) return
+        if (collidedWith != owner && this.age <= ONSConfig.INSTANCE.orbOwnerTime.get()) return
+        if (variant.supplyLoot(collidedWith, this.pos))
+            this.discard()
+    }
+
+    override fun setOwner(entity: Entity) {
+        this.owner = owner
+    }
+
     override fun getOwner(): Entity? {
-        TODO("Not yet implemented")
+        return owner
     }
 }
