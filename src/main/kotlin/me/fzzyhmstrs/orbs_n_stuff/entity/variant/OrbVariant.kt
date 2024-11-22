d04a57127aa6f5a7a17b79db5f02a24e4695f43f
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2024-11-22 12:33:10 -0500

    Update OrbVariant.kt

--- src/main/kotlin/me/fzzyhmstrs/orbs_n_stuff/entity/variant/OrbVariant.kt
+++ src/main/kotlin/me/fzzyhmstrs/orbs_n_stuff/entity/variant/OrbVariant.kt
@@ -1,7 +1,9 @@
 package me.fzzyhmstrs.orbs_n_stuff.entity.variant
 
 import me.fzzyhmstrs.lootables.api.LootableItem
+import net.minecraft.server.network.ServerPlayerEntity
+import java.util.function.Consumer
 import java.util.function.Supplier
 
-data class OrbVariant(val data: LootableItem.LootableData, val color: Supplier<Int>) {
+data class OrbVariant(val data: LootableItem.LootableData, val color: Supplier<Int>, val onPickedUp: Consumer<ServerPlayerEntity>) {
 }
